3e13549ad45fd148fafc7e602b0a2544c3f29138
alngo <alngo@student.42.fr>
2020-01-27 15:03:03 +0100

    feat(travis): setup travis.

---
+++ ci/srcs/main.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alngo <alngo@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/01/27 14:50:22 by alngo             #+#    #+#             */
+/*   Updated: 2020/01/27 14:51:40 by alngo            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "test.h"
+
+MU_TEST(test_check)
+{
+	mu_check(5 == 7);
+}
+
+MU_TEST_SUITE(test_suite)
+{
+	MU_RUN_TEST(test_check);
+}
+
+int main(int argc, char *argv[]) {
+	MU_RUN_SUITE(test_suite);
+	MU_REPORT();
+	return MU_EXIT_CODE;
+}
