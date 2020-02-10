13a5c5c7a1446b2569266c30db2537cda7bb319b
alngo <alngo@student.42.fr>
2020-02-10 16:41:35 +0100

    feat(malloc): remove unused comparison

--- srcs/malloc.c
+++ srcs/malloc.c
@@ -6,7 +6,7 @@
 /*   By: alngo <alngo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/27 13:18:41 by alngo             #+#    #+#             */
-/*   Updated: 2020/02/10 14:26:16 by alngo            ###   ########.fr       */
+/*   Updated: 2020/02/10 16:40:53 by alngo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ void		*malloc(size_t size)
 {
 	void	*block;
 
-	if (size < 0 || size > (~(size_t)0 >> 3))
+	if (size > (~(size_t)0 >> 3))
 		return (NULL);
 	if (size <= TINY)
 		block = get_block(&g_arena.tiny, size);
