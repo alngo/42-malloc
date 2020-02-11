da4285e25d74101b8f7a17d7a2e1d4354f131ad5
alngo <alngo@student.42.fr>
2020-02-11 15:19:35 +0100

    feat(tests): restructure test architecture.

---
+++ ci/tests42/run_tests.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+lft=../libft_malloc.so
+out=./out
+
+echo "==================================="
+echo "nm $lft"
+echo "==================================="
+/usr/bin/nm $lft
+
+echo "==================================="
+echo "/usr/bin/time -l $out/test0"
+echo "==================================="
+/usr/bin/time -l $out/test0
+
+echo "==================================="
+echo "/usr/bin/time -l $out/test1"
+echo "==================================="
+/usr/bin/time -l $out/test1
+
+echo "==================================="
+echo "./run.sh /usr/bin/time -l $out/test1"
+echo "==================================="
+./run.sh /usr/bin/time -l $out/test1
+
+echo "==================================="
+echo "./run.sh /usr/bin/time -l $out/test2"
+echo "==================================="
+./run.sh /usr/bin/time -l $out/test2
+
+echo "==================================="
+echo "./run.sh /usr/bin/time -l $out/test3"
+echo "==================================="
+./run.sh $out/test3
+
+echo "==================================="
+echo "./run.sh /usr/bin/time -l $out/test3bis"
+echo "==================================="
+./run.sh $out/test3bis
+
+echo "==================================="
+echo "./run.sh /usr/bin/time -l $out/test4"
+echo "==================================="
+./run.sh $out/test4
+
+echo "==================================="
+echo "./run.sh /usr/bin/time -l $out/test5"
+echo "==================================="
+./$out/test5
--- ci/units.sh
+++ ci/units.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 expected_exit_code=0
-./test.out
+./units.out
 if [ $? -ne $expected_exit_code ]; then
   echo "Unexpected exit code: $expected_exit_code expected but was $?"
   exit 1
 fi
+./tests42/run_tests.sh
 exit 0
